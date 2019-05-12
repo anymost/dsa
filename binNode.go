0a20372d3545d908ba4f2254e01400604ccc741e
anymost <creatuture@gmail.com>
2019-05-12 12:51:39 +0800

    update binNode and binTree

---
+++ binNode.go
@@ -0,0 +1,81 @@
+package main
+
+type BinNode struct {
+	parent, leftChild, rightChild *BinNode
+	data                          int
+	height                        int
+}
+
+func (binNode *BinNode) Size() int {
+	size := 1
+	if binNode.leftChild != nil {
+		size += binNode.leftChild.Size()
+	}
+	if binNode.rightChild != nil {
+		size += binNode.rightChild.Size()
+	}
+	return size
+}
+
+func (binNode *BinNode) InsertAsLeft(val int) *BinNode {
+	binNode.leftChild = &BinNode{
+		parent: binNode,
+		data:   val,
+	}
+	return binNode.leftChild
+}
+
+func (binNode *BinNode) InsertAsRight(val int) *BinNode {
+	binNode.rightChild = &BinNode{
+		parent: binNode,
+		data:   val,
+	}
+	return binNode.rightChild
+}
+
+func (binNode *BinNode) Succ() *BinNode {
+	return &BinNode{}
+}
+
+func (binNode *BinNode) TravelLevel(callback func(node *BinNode)) {
+
+}
+
+func (binNode *BinNode) TravelPre(callback func(node *BinNode)) {
+}
+
+func (binNode *BinNode) TravelIn(callback func(node *BinNode)) {
+
+}
+
+func (binNode *BinNode) TravelPost(callback func(node *BinNode)) {
+
+}
+
+func Traverse(node *BinNode, callback func(val int)) {
+	if node == nil {
+		return
+	}
+	callback(node.data)
+	Traverse(node.leftChild, callback)
+	Traverse(node.rightChild, callback)
+}
+
+func TraverseT1(node *BinNode, callback func(val int)) {
+	if node == nil {
+		return
+	}
+	stack := NewStack()
+	stack.Push(node)
+	for !stack.Empty() {
+		node, _ = stack.Pop().(*BinNode)
+		callback(node.data)
+		// 先右后左
+		if node.rightChild != nil {
+			stack.Push(node.rightChild)
+		}
+		if node.leftChild != nil {
+			stack.Push(node.leftChild)
+		}
+	}
+}
--- binTree.go
+++ binTree.go
@@ -1,68 +1,54 @@
 package main
 
+import "math"
+
 // 边数之和等于顶点数之和减一
 // 满二叉树节点为2^(h+1)-1, h为高度
 // 真二叉树：不存在子节点数为1的节点
 // 对于有根有序树，通过长子兄弟表示法，将多叉树转化为二叉树
 
-type BinNode struct {
-	parent, leftChild, rightChild *BinNode
-	data                          int
-	height                        int
-}
 
 type BinTree struct {
 	size int
 	root *BinNode
 }
 
-func (binNode *BinNode) Size() int {
-	size := 1
-	if binNode.leftChild != nil {
-		size += binNode.leftChild.Size()
-	}
-	if binNode.rightChild != nil {
-		size += binNode.rightChild.Size()
-	}
-	return size
-}
-
-func (binNode *BinNode) InsertAsLeft(val int) *BinNode {
-	binNode.leftChild = &BinNode{
-		parent: binNode,
-		data: val,
-	}
-	return binNode.leftChild
-}
-
-func (binNode *BinNode) InsertAsRight(val int) *BinNode {
-	binNode.rightChild = &BinNode{
-		parent: binNode,
-		data: val,
-	}
-	return binNode.rightChild
-}
-
-func (binNode *BinNode) Succ() *BinNode {
-	return &BinNode{}
-}
-
-func (binNode *BinNode) TravelLevel(callback func(node *BinNode)) {
-
-}
-
-func (binNode *BinNode) TravelPre(callback func(node *BinNode)) {
-
-}
-
-func (binNode *BinNode) TravelIn(callback func(node *BinNode)) {
-
-}
-
-func (binNode *BinNode) TravelPost(callback func(node *BinNode)) {
-
-}
-
 func (binTree *BinTree) Empty() bool {
 	return binTree.size == 0
+}
+
+func (binTree *BinTree) UpdateHeight(node *BinNode) int {
+	leftHeight := float64(structure(node.leftChild))
+	rightHeight := float64(structure(node.rightChild))
+	node.height = int(math.Max(leftHeight, rightHeight)) + 1
+	return node.height
+}
+
+func (binTree *BinTree) UpdateHeightAbove(node *BinNode) {
+	for node != nil {
+		binTree.UpdateHeight(node)
+		node = node.parent
+	}
+}
+
+func (binTree *BinTree) InsertAsLeft(node *BinNode, val int) *BinNode {
+	binTree.size++
+	node.InsertAsLeft(val)
+	binTree.UpdateHeightAbove(node)
+	return node.leftChild
+}
+
+func (binTree *BinTree) InsertAsRight(node *BinNode, val int) *BinNode {
+	binTree.size++
+	node.InsertAsRight(val)
+	binTree.UpdateHeightAbove(node)
+	return node.rightChild
+}
+
+func structure(p *BinNode) int {
+	if p == nil {
+		return -1
+	} else {
+		return p.height
+	}
 }
