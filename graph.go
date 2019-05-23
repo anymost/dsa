86a5ae759e4b990250790418da32a08fd4d92000
anymost <creatuture@gmail.com>
2019-05-23 23:09:42 +0800

    add Vertex,Edge,GraphMatrix

---
+++ graph.go
@@ -0,0 +1,69 @@
+package main
+
+import "math"
+
+type VStatus int
+type EStatus int
+
+const (
+	UNDISCOVERED VStatus = iota
+	DISCOVERED
+	VISITED
+)
+
+const (
+	UNDETERMINED EStatus = iota
+	TREE
+	CROSS
+	FORWARD
+	BACKWARD
+)
+
+type Vertex struct {
+	data      int
+	status    VStatus
+	inDegree  int
+	outDegree int
+	dTime     int
+	fTime     int
+	parent    int
+	priority  int
+}
+
+type Edge struct {
+	data   int
+	weight int
+	status EStatus
+}
+
+type GraphMatrix struct {
+	vertex []Vertex
+	edge   [][]Edge
+}
+
+func NewVertex(data int) *Vertex {
+	return &Vertex{
+		data:      data,
+		inDegree:  0,
+		outDegree: 0,
+		status:    UNDISCOVERED,
+		parent:    -1,
+		fTime:     math.MaxInt32,
+		dTime:     math.MaxInt32,
+	}
+}
+
+func NewEdge(data int, weight int) *Edge {
+	return &Edge{
+		data:   data,
+		weight: weight,
+		status: UNDETERMINED,
+	}
+}
+
+func NewGraphMatrix() *GraphMatrix {
+	return &GraphMatrix{
+		vertex: make([]Vertex, 0),
+		edge:   make([][]Edge, 0),
+	}
+}
